902c5a3297153c80b96f751161d73f5ae35def64
Alina <rattoden@gmail.com>
2023-01-31 17:56:40 +0300

    Добавьте файлы проекта.

---
+++ helloapp/helloapp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using helloapp.Models;
+using helloapp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace helloapp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
